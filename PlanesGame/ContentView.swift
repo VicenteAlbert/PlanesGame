a6243c1f14ccd256f691e0fdc217e2bfa643a0f5
Vicentiu Petreaca <vpetreaca@fitbit.com>
2021-01-05 18:21:11 +0200

    Initial Commit

---
+++ PlanesGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlanesGame
+//
+//  Created by Vicentiu Petreaca on 05/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlanesGame/PlanesGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanesGameApp.swift
+//  PlanesGame
+//
+//  Created by Vicentiu Petreaca on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlanesGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
